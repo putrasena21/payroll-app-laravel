6df50ab595c8652c71d0dfaff8212df69e7433ec
moch faisal khoirudin <putrasena21@gmail.com>
2022-09-20 17:59:17 +0700

    create table verification token

---
+++ database/migrations/2022_09_20_095332_create_verification_tokens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('token')->unique();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('user_hrds')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification_tokens');
+    }
+};
